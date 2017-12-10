cf71aff9a5c01c1064c13bd9074d319b94a45b3a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-10 18:16:06 +0100

    Process all

--- static/tool.js
+++ static/tool.js
@@ -39,13 +39,14 @@ function sendForm() {
 		var id = Number(image.name.replace('description-', ''));
 		console.log(id);
 		var description = image.value;
-		var imagepayload = {
-			'id': id,
-			'description': description,
-			'lang': language,
-		};
-		payload.push(imagepayload);
-		break;
+		if (description!='') {
+			var imagepayload = {
+				'id': id,
+				'description': description,
+				'lang': language,
+			};
+			payload.push(imagepayload);
+		}
 	}
 	$.postJSON('https://tools.wmflabs.org/commons-mass-description/api-edit', payload, function (data) {
 		console.log(data);
