b29de336ae59948e1f7082eeac67c64358178678
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-10 16:47:12 +0100

    Tool will be rewritten

--- static/tool.js
+++ static/tool.js
@@ -14,6 +14,8 @@ function escapeHtml (string) {
 	return entityMap[s];
   });
 }
+
+
 $.getJSON('https://tools.wmflabs.org/commons-mass-description/api-langs', function (data) {
 	for (var i = 0; i < data['langs'].length; i++) {
 		if (data['langs'][i]['code'] == 'cs') {
@@ -25,46 +27,3 @@ $.getJSON('https://tools.wmflabs.org/commons-mass-description/api-langs', functi
 		$('#langs').append(row);
 	}
 })
-fillPics();
-
-function fillPics() {
-	//var url = 'https://tools.wmflabs.org/commons-mass-description/api-images?offset='+$('#offset').text();
-	var url = 'https://tools.wmflabs.org/commons-mass-description/api-images'; // Debug, just to test if the rest works
-	$.getJSON(url, function (data) {
-		for (var i = 0; i < data['images'].length; i++) {
-			var row = '<div class="imagerow"><tr><td><img class="image" alt="' + escapeHtml(data['images'][i]['title']) + '" src="' + data['images'][i]['thumburl'] + '"></td><td><input type="text" class="description" id="description-' + i + '"></td></div>';
-			$('tbody').append(row);
-			break;
-		}
-		$('button')[0].disabled = false;
-	})
-}
-function sendForm() {
-	var data = $('.description');
-	$('tbody').empty();
-	$('button')[0].disabled = true;
-	swal("Momentálně vaše data zpracováváme!", "Počkejte, prosím!");
-	var request = [];
-	for (var i = 0; i < data.length; i++) {
-		var rowdata = {};
-		rowdata['description'] = data[i].value;
-		rowdata['title'] = data[i].closest('.imagerow').children[0].alt;
-		rowdata['lang'] = $('#langs')[0].value;
-		request.push(rowdata);
-	}
-	/*$.ajax({
-		type: 'POST',
-		url: 'https://tools.wmflabs.org/commons-mass-description/api-edit',
-		data: JSON.stringify(request),
-		success: function (data) {
-			console.log(data);
-		},
-		error: function (request, error) {
-			console.log(error);
-		},
-		contentType: "application/json",
-		dataType: "json"
-	})*/
-	$('#offset').text(Number($('#offset').text())+1);
-	fillPics();
-}
