75cdc922c082c3ae36c477ebc2471c959572cce2
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-10 20:39:09 +0100

    Require document.ready

--- static/all.js
+++ static/all.js
@@ -1,19 +1,21 @@
-$.getJSON('https://tools.wmflabs.org/commons-mass-description/api-username', function (data) {
-	$('#username').text(data['username']);
+$( document ).ready(function() {
+	$.getJSON('https://tools.wmflabs.org/commons-mass-description/api-username', function (data) {
+		$('#username').text(data['username']);
+	});
+
+	$.postJSON = function(url, data, callback) {
+	    return jQuery.ajax({
+	        'type': 'POST',
+	        'url': url,
+	        'contentType': 'application/json',
+	        'data': JSON.stringify(data),
+	        'dataType': 'json',
+	        'success': callback
+	    });
+	};
+
+	String.prototype.replaceAll = function(search, replacement) {
+	    var target = this;
+	    return target.split(search).join(replacement);
+	};
 });
-
-$.postJSON = function(url, data, callback) {
-    return jQuery.ajax({
-        'type': 'POST',
-        'url': url,
-        'contentType': 'application/json',
-        'data': JSON.stringify(data),
-        'dataType': 'json',
-        'success': callback
-    });
-};
-
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-};
