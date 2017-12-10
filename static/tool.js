c41f1d248e8c14d67644c6863bcf38468b17ca17
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-10 17:06:42 +0100

    Use offset hidely

--- static/tool.js
+++ static/tool.js
@@ -34,11 +34,11 @@ function sendForm() {
 }
 
 function fillPics() {
+	var url = 'https://tools.wmflabs.org/commons-mass-description/api-images?offset=' + $('#offset').text()
 	$.getJSON('https://tools.wmflabs.org/commons-mass-description/api-images', function (data) {
 		for (var i = 0; i < data.images.length; i++) {
 			var image = data.images[i];
 			$('tbody').append('<tr><td><img src="' + image.thumburl + '"></td><td><input type="text" name="description-' + image.id + '"></td></tr>');
-			break;
 		}
 		$('button')[0].disabled = false;
 	});
