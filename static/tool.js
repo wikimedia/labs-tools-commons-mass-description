ed89e3a438ea76d0df7a2f1754caa750dccd6204
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-12 22:41:24 +0100

    Add some reliable ids

--- static/tool.js
+++ static/tool.js
@@ -51,7 +51,7 @@ function fillPics() {
 	$.getJSON(url, function (data) {
 		for (var i = 0; i < data.images.length; i++) {
 			var image = data.images[i];
-			$('tbody').append('<tr><td><a href="' + image.url + '" data-lightbox="image-' + image.id + '"><img src="' + image.thumburl + '"></a></td><td><input type="text" name="description-' + image.id + '"></td></tr>');
+			$('tbody').append('<tr id="image-row-' + image.id + '"><td><a href="' + image.url + '" data-lightbox="image-' + image.id + '"><img src="' + image.thumburl + '"></a></td><td><input type="text" name="description-' + image.id + '"></td></tr>');
 		}
 		$('#send')[0].disabled = false;
 		$('#search')[0].disabled = false;
